2bbafb664c51cf40c915caea48977d695ec7a4c4
Ramiro Berrelleza <ramiro@okteto.com>
2023-10-25 14:23:21 -0700

    use a different dockerconfig to avoid conflicts

--- hacks/clean-auth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# this is needed due to ko not being able to handle custom credStores, which Okteto uses
-if [ ! -z "$OKTETO_IN_INSTALLER" ] || [ ! -z "$OKTETO_DEPLOY_REMOTE" ]; then
-  echo  "commands are running in okteto, deleting existing docker config"
-  rm /root/.docker/config.json
-fi
-
